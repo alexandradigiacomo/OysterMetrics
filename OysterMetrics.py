c4823aadc4a09b9c0a6b83c2a712818f1fc9a54b
alexandra digiacomo <aed41@duke.edu>
2020-11-20 09:50:05 -0500

    commit 11/20

--- OysterMetrics.py
+++ OysterMetrics.py
@@ -24,48 +24,56 @@ arcpy.env.workspace = dataWS
 arcpy.env.overwriteOutput = True
 
 ## USER INPUTS
-scratchWS = arcpy.GetParameterAsText(0) #scratch folder
-arcpy.env.scratchWorkspace = scratchWS #set the environmental variable for the scratch folder after knowing the scratch folder
+input_scratchWS = arcpy.GetParameterAsText(0) # input scratch folder
+arcpy.env.scratchWorkspace = input_scratchWS #set the environmental variable for the scratch folder after knowing the scratch folder
 #input files
 input_dsm = arcpy.GetParameterAsText(1) #input oyster dsm
-input_orthomosaic = arcpy.GetParameterAsText(2) # input oyster ortho
-input_reefPolygon = arcpy.GetParameterAsText(3) # line along reef edge
+input_reefPolygon = arcpy.GetParameterAsText(2) # line along reef edge
+input_reefName = arcpy.GetParameterAsText(3) # enter the reef name, no spaces
+input_sampleNum = arcpy.GetParameterAsText(4) # enter the number of samples you want to pick
 
-# 0. Get Parent Arc Folder (assuming scratch nested within - change this to
-# more adaptable to other users later)
-ArcPar = os.path.dirname(scratchWS) # get arc parent folder
+# Determine Parent Arc Folder (assuming scratch nested within - change this to more adaptable to other users later)
+ArcPar = os.path.dirname(input_scratchWS) # get arc parent folder
 arcpy.AddMessage(ArcPar)
 
-# 1. Buffer input reef polygon (outside)
+# Create subfolder based on reef name
+## allows you to save files for each reef rather than overwriting each time
+scratchWS = input_scratchWS + "\\" + input_reefName # set scratch workspace to subfolder with reef name
+if not os.path.exists(scratchWS):
+    os.makedirs(scratchWS)
+arcpy.AddMessage("folder for this reef created at: " + scratchWS)
+
+# Create path to gdb
 gdb = ArcPar + "\\OysterMetrics.gdb" # get path of geodatabase
 
+# 1. Normalize DSM plane
+
+# a. Buffer input reef polygon (outside)
 outBufferReefEdge = gdb + "\\outerBufferReef" # save within geodatabase
 arcpy.Buffer_analysis(input_reefPolygon, outBufferReefEdge, "0.25 Meters")
-arcpy.AddMessage("outer reef edge buffer created")
+arcpy.AddMessage("(1a) outer reef edge buffer created")
 
-# 2. Convert Outer Buffer Polygon to line
+# b. Convert Outer Buffer Polygon to line
 outBufferReefLine = gdb + "\\outerBufferLine"
 arcpy.PolygonToLine_management(outBufferReefEdge, outBufferReefLine) # create outer buffer line
-arcpy.AddMessage("outer reef buffer converted to line")
+arcpy.AddMessage("(1b) outer reef buffer converted to line")
 
-# 3. Generate Samples along line
+# c. Generate Samples along line
 normalizeSamplePoints = gdb + "\\normalizeSamplePts"
 arcpy.GeneratePointsAlongLines_management(outBufferReefLine, normalizeSamplePoints,
     'PERCENTAGE', Percentage = 10)
-arcpy.AddMessage("normalizing samples created")
+arcpy.AddMessage("(1c) normalizing samples created")
 
-# 4. Extract DSM Values at Sample Points
-
-# a. Copy Features to prepare points with object IDs
+# d. Copy Features to prepare points with object IDs
 normalizeSamplePoints_CF = gdb + "\\normalizeSamplePtsCF"
 arcpy.CopyFeatures_management(normalizeSamplePoints, normalizeSamplePoints_CF)
 
-# b. Extract values to points
+# e. Extract values to points
 normalizeSamplePts_DSMAttached = gdb + "\\normalizeSamplePts_DSMAttached"
 ExtractValuesToPoints(normalizeSamplePoints_CF, input_dsm, normalizeSamplePts_DSMAttached)
-arcpy.AddMessage("normalizing samples extracted DSM values")
+arcpy.AddMessage("(1d) normalizing samples extracted DSM values")
 
-# 5. Create Plane from Sample Point DSM values)
+# f. Create Plane from Sample Point DSM values)
 normalPlane = scratchWS + "\\normalPlane.tif" # delete previous version
 if os.path.exists(normalPlane):
     os.remove(normalPlane)
@@ -73,9 +81,9 @@ if os.path.exists(normalPlane):
 normalPlane = scratchWS + "\\normalPlane.tif"
 outTrend = Trend(normalizeSamplePts_DSMAttached, "RASTERVALU", order = 1)
 outTrend.save(normalPlane)
-arcpy.AddMessage("normalized plane generated and saved:" + normalPlane)
+arcpy.AddMessage("(1e) normalized plane generated and saved:" + normalPlane)
 
-# 6. Subtract normalized plane from DSM to create normalized dsm
+# g. Subtract normalized plane from DSM to create normalized dsm
 normalDSM = scratchWS + "\\normalDSM.tif" # delete previous version
 if os.path.exists(normalDSM):
     os.remove(normalDSM)
@@ -83,35 +91,46 @@ if os.path.exists(normalDSM):
 normalDSM = scratchWS + "\\normalDSM.tif"
 outMinus = Raster(input_dsm) - Raster(normalPlane)
 outMinus.save(normalDSM)
-arcpy.AddMessage("DSM normalized to height and saved:" + normalDSM)
+arcpy.AddMessage("(1f) DSM normalized to height and saved:" + normalDSM)
 
-# 7. Downsample normalized DSM to 2xGSD
+### DSM NORMALIZED ####
+
+# 2. Downsample normalized DSM to 2xGSD
 normalDSMx2 = scratchWS + "\\normalDSMx2.tif"
 gsdx = arcpy.GetRasterProperties_management(normalDSM, property_type = "CELLSIZEX") # original gsdx
 gsdy = arcpy.GetRasterProperties_management(normalDSM, property_type = "CELLSIZEY") # original gsdy
 gsd = float(gsdx[0])*2 # target GSD; 2x original GSD
 arcpy.Resample_management(normalDSM, normalDSMx2, gsd, "BILINEAR") # use gsd as an input for cell size
-arcpy.AddMessage("normalized DSM resampled to 2xGSD: " + str(gsd))
+arcpy.AddMessage("(2) normalized DSM resampled to 2xGSD: " + str(gsd))
 
-## Take Reef Samples from Normalized Reef DSM
+### DSM DOWNSAMPLED ###
+
+## 3. Take Reef Samples from Normalized Reef DSM
 
 ## Edge Samples
-
-# 7. Buffer input reef polygon (inside)
+# a. Buffer input reef polygon (inside)
 innerBufferReefEdge = gdb + "\\innerBufferReef"
-arcpy.Buffer_analysis(input_reefPolygon, innerBufferReefEdge, "-0.25 Meters")
-arcpy.AddMessage("inner reef edge buffer created")
+arcpy.Buffer_analysis(input_reefPolygon, innerBufferReefEdge, "-0.35 Meters") # buffer in by diagonal length
+arcpy.AddMessage("(3a) inner reef edge buffer created")
 
-# 8. Convert Inner Buffer Polygon to line
+# b. Convert Inner Buffer Polygon to line
 innerBufferReefLine = gdb + "\\innerBufferLine"
 arcpy.PolygonToLine_management(innerBufferReefEdge, innerBufferReefLine) # create inner buffer line
-arcpy.AddMessage("inner reef buffer converted to line")
+arcpy.AddMessage("(3b) inner reef buffer converted to line")
 
-# 9. Generate Samples along inner reef edge
+# c. Generate Samples along inner reef edge
 reefEdgeSamplePoints = gdb + "\\reefEdgeSamplePoints"
-arcpy.GeneratePointsAlongLines_management(innerBufferReefLine, reefEdgeSamplePoints,
-    'PERCENTAGE', Percentage = 10)
-arcpy.AddMessage("edge sample points created")
+
+if (int(input_sampleNum) == 4):
+    arcpy.AddMessage("Default, 4 Samples chosen")
+    arcpy.management.GeneratePointsAlongLines(innerBufferReefLine, reefEdgeSamplePoints,
+        'PERCENTAGE', None, 25, None) # take four edge samples
+else :
+    arcpy.AddMessage("The default was not chosen...")
+
+#arcpy.management.GeneratePointsAlongLines(innerBufferReefLine, reefEdgeSamplePoints,
+    #'DISTANCE', "0.4 Meters") # separate each by the diagonal length (0.35) + buffer for separation
+#arcpy.AddMessage("(3c) edge sample points created")
 
 # 10. Buffer reef edge Samples
 reefEdgeSamplesBuffered = gdb + "\\reefEdgeSamplesBuffered"
@@ -194,9 +213,11 @@ arcpy.AddMessage("crest sample points converted to square buffer")
 
 ## METRICS
 tables = scratchWS + "\\tables" # output location for metric tables
+if not os.path.exists(tables):
+    os.makedirs(tables)
 
-# 21. elevation
-# a. Extract elevation statistics at edge plots
+# 1. Elevation
+# a. Edge
 elevationEdgeDBF = tables + "\\elevationEdge.dbf"
 arcpy.sa.ZonalStatisticsAsTable(reefEdgeSamplesSquareBuf, "OBJECTID", normalDSMx2, elevationEdgeDBF,
     "DATA", "MEAN")
@@ -205,7 +226,7 @@ elevationEdge = tables + "\\elevationEdge.csv"
 arcpy.conversion.TableToTable(elevationEdgeDBF, tables, "elevationEdge.csv") # convert to csv format
 arcpy.AddMessage("zonal statistics: elevation at reef edge exported")
 
-# b. Extract elevation statistics at crest Plots
+# b. Crest
 elevationCrestDBF = tables + "\\elevationCrest.dbf"
 arcpy.sa.ZonalStatisticsAsTable(reefCrestSamplesSquareBuf, "OBJECTID", normalDSMx2, elevationCrestDBF,
     "DATA", "MEAN")
@@ -214,7 +235,7 @@ elevationCrest = tables + "\\elevationCrest.csv"
 arcpy.conversion.TableToTable(elevationCrestDBF, tables, "elevationCrest.csv") # convert to csv format
 arcpy.AddMessage("zonal statistics: elevation at reef crest exported")
 
-# 22. Mean Slope
+# 2. Mean Slope
 
 # Create Slope Raster
 slopeDSM = tables + "//slopeDSM.tif"
@@ -222,16 +243,16 @@ outSlopeDSM = arcpy.sa.Slope(normalDSMx2, "DEGREE", 1, "PLANAR", "METER")
 outSlopeDSM.save(slopeDSM) # save the slope raster
 arcpy.AddMessage("slope raster generated")
 
-# a. Extract slope statistics at edge Plots
+# a. Edge
 slopeEdgeDBF = tables + "\\slopeEdge.dbf"
 arcpy.sa.ZonalStatisticsAsTable(reefEdgeSamplesSquareBuf, 'OBJECTID', slopeDSM, slopeEdgeDBF,
     "DATA", "MEAN")
 
-slopeEdge = tables + "\\elevationEdge.csv"
+slopeEdge = tables + "\\slopeEdge.csv"
 arcpy.conversion.TableToTable(slopeEdgeDBF, tables, "slopeEdge.csv") # convert to csv format
 arcpy.AddMessage("zonal statistics: slope at reef edge exported")
 
-# b. Extract slope statistics at Crest plots
+# b. Slope
 slopeCrestDBF = tables + "\\slopeCrest.dbf"
 arcpy.sa.ZonalStatisticsAsTable(reefCrestSamplesSquareBuf, 'OBJECTID', slopeDSM, slopeCrestDBF,
     "DATA", "MEAN")
@@ -240,12 +261,11 @@ slopeCrest = tables + "\\slopeCrest.csv"
 arcpy.conversion.TableToTable(slopeCrestDBF, tables, "slopeCrest.csv") # convert to csv format
 arcpy.AddMessage("zonal statistics: slope at reef crest exported")
 
-# #23. Variance in Slope
+# 3. Standard Deviation of Slope
 # a. Edge
 slopeStdEdgeDBF = tables + "\\slopeStdEdge.dbf"
 arcpy.sa.ZonalStatisticsAsTable(reefEdgeSamplesSquareBuf, 'OBJECTID', slopeDSM, slopeStdEdgeDBF,
     "DATA", "STD")
-
 slopeStdEdge = tables + "\\slopeStdEdge.csv"
 arcpy.conversion.TableToTable(slopeStdEdgeDBF, tables, "slopeStdEdge.csv") # convert to csv format
 arcpy.AddMessage("zonal statistics: slope standard deviation at reef edge exported")
@@ -254,7 +274,6 @@ arcpy.AddMessage("zonal statistics: slope standard deviation at reef edge export
 slopeStdCrestDBF = tables + "\\slopeStdCrest.dbf"
 arcpy.sa.ZonalStatisticsAsTable(reefCrestSamplesSquareBuf, 'OBJECTID', slopeDSM, slopeStdCrestDBF,
     "DATA", "STD")
-
 slopeStdCrest = tables + "\\slopeStdCrest.csv"
 arcpy.conversion.TableToTable(slopeStdCrestDBF, tables, "slopeStdCrest.csv") # convert to csv format
 arcpy.AddMessage("zonal statistics: slope standard deviation at reef crest exported")
@@ -275,12 +294,54 @@ arcpy.ddd.AddSurfaceInformation(reefCrestSamplesSquareBuf, normalDSMx2,
 arcpy.management.CopyRows(reefCrestSamplesSquareBuf, rugosityCrest, None)
 arcpy.AddMessage("rugosity at reef crest exported")
 
+# 4. Curvature
+
+# generate curvature rasters
+profileCurv = scratchWS + "\\profileCurv.tif"
+planformCurv = scratchWS + "\\planformCurv.tif"
+genCurv = scratchWS + "\\generalCurv.tif"
+arcpy.ddd.Curvature(normalDSMx2, genCurv, 1, profileCurv, planformCurv)
+arcpy.AddMessage("curvature rasters generated")
+
+# a. Edge
+# profile
+profileCurvEdgeDBF = tables + "\\profileCurvEdge.dbf"
+arcpy.sa.ZonalStatisticsAsTable(reefEdgeSamplesSquareBuf, 'OBJECTID', profileCurv,
+    profileCurvEdgeDBF, "DATA", "MEAN")
+profileCurvEdge = tables + "\\profileCurvEdge.csv"
+arcpy.conversion.TableToTable(profileCurvEdgeDBF, tables, "profileCurvEdge.csv") # convert to csv format
+arcpy.AddMessage("zonal statistics: profile curvature at edge exported")
+
+# planform
+planformCurvEdgeDBF = tables + "\\planformCurvEdge.dbf"
+arcpy.sa.ZonalStatisticsAsTable(reefEdgeSamplesSquareBuf, 'OBJECTID', planformCurv,
+    planformCurvEdgeDBF, "DATA", "MEAN")
+planformCurvEdge = tables + "\\planformCurvEdge.csv"
+arcpy.conversion.TableToTable(planformCurvEdgeDBF, tables, "planformCurvEdge.csv") # convert to csv format
+arcpy.AddMessage("zonal statistics: planform curvature at edge exported")
+
+# b. Crest
+# profile
+profileCurvCrestDBF = tables + "\\profileCurvCrest.dbf"
+arcpy.sa.ZonalStatisticsAsTable(reefCrestSamplesSquareBuf, 'OBJECTID', profileCurv,
+    profileCurvCrestDBF, "DATA", "MEAN")
+profileCurvCrest = tables + "\\profileCurvCrest.csv"
+arcpy.conversion.TableToTable(profileCurvCrestDBF, tables, "profileCurvCrest.csv")
+arcpy.AddMessage("zonal statistics: profile curvature at crest exported")
+
+# planform
+planformCurvCrestDBF = tables + "\\planformCurvCrest.dbf"
+arcpy.sa.ZonalStatisticsAsTable(reefCrestSamplesSquareBuf, 'OBJECTID', planformCurv,
+    planformCurvCrestDBF, "DATA", "MEAN")
+planformCurvCrest = tables + "\\planformCurvCrest.csv"
+arcpy.conversion.TableToTable(planformCurvCrestDBF, tables, "planformCurvCrest.csv") # convert to csv format
+arcpy.AddMessage("zonal statistics: planform curvature at crest exported")
+
 ### End Metric Extraction ###
 
 # Data Analysis
 
 # 1. Elevation
-
 # a. Edge
 elevation_edge = pd.read_csv(elevationEdge)
 elevation_edge = elevation_edge[["OBJECTID", "MEAN"]]
@@ -295,7 +356,7 @@ elevation_crest = elevation_crest.assign(metric = "elevation", area = "crest")
 elevation_crest.columns = ['value' if x == 'MEAN' else x for x in elevation_crest.columns]
 arcpy.AddMessage(elevation_crest)
 
-# 2. Slope
+# 2. Mean Slope
 
 # a. Mean Slope, Edge
 slope_edge = pd.read_csv(slopeEdge)
@@ -309,20 +370,21 @@ slope_crest = slope_crest[["OBJECTID", "MEAN"]]
 slope_crest = slope_crest.assign(metric = "slope", area = "crest")
 slope_crest.columns =  ['value' if x == 'MEAN' else x for x in slope_crest.columns]
 
-# c. Standard Deviation of Slope, Edge
+# 3. Slope Standard Deviation
+
+# a. Standard Deviation of Slope, Edge
 slopeStd_edge = pd.read_csv(slopeStdEdge)
 slopeStd_edge = slopeStd_edge[["OBJECTID", "STD"]]
 slopeStd_edge = slopeStd_edge.assign(metric = "slopestd", area = "edge")
 slopeStd_edge.columns =  ['value' if x == 'STD' else x for x in slopeStd_edge.columns]
 
-# d. Standard Deviation of Slope, Crest
+# b. Standard Deviation of Slope, Crest
 slopeStd_crest = pd.read_csv(slopeStdCrest)
 slopeStd_crest = slopeStd_crest[["OBJECTID", "STD"]]
 slopeStd_crest = slopeStd_crest.assign(metric = "slopestd", area = "crest")
 slopeStd_crest.columns =  ['value' if x == 'STD' else x for x in slopeStd_crest.columns]
 
-
-# 3. Rugosity
+# 4. Rugosity
 
 # a. Edge
 rugosity_edge = pd.read_csv(rugosityEdge)
@@ -338,13 +400,50 @@ rugosity_crest = rugosity_crest.assign(metric = "rugosity", area = "crest")
 rugosity_crest["SArea"] = rugosity_crest["SArea"]/0.25 # make rugosity ratio (3D SA (SArea) / 2D SA (0.25m))
 rugosity_crest.columns = ['value' if x == 'SArea' else x for x in rugosity_crest.columns]
 
+# 5. Curvature
+
+# a. Edge
+# planform
+planformcurvature_edge = pd.read_csv(planformCurvEdge)
+planformcurvature_edge = planformcurvature_edge[["OBJECTID", "MEAN"]]
+planformcurvature_edge = planformcurvature_edge.assign(metric = "planformcurvature", area = "edge")
+planformcurvature_edge.columns =  ['value' if x == 'MEAN' else x for x in planformcurvature_edge.columns]
+
+# profile
+profilecurvature_edge = pd.read_csv(profileCurvEdge)
+profilecurvature_edge = profilecurvature_edge[["OBJECTID", "MEAN"]]
+profilecurvature_edge = profilecurvature_edge.assign(metric = "profilecurvature", area = "edge")
+profilecurvature_edge.columns =  ['value' if x == 'MEAN' else x for x in profilecurvature_edge.columns]
+
+# b. Crest
+planformcurvature_crest = pd.read_csv(planformCurvCrest)
+planformcurvature_crest = planformcurvature_crest[["OBJECTID", "MEAN"]]
+planformcurvature_crest = planformcurvature_crest.assign(metric = "planformcurvature", area = "crest")
+planformcurvature_crest.columns =  ['value' if x == 'MEAN' else x for x in planformcurvature_crest.columns]
+
+# profile
+profilecurvature_crest = pd.read_csv(profileCurvCrest)
+profilecurvature_crest = profilecurvature_crest[["OBJECTID", "MEAN"]]
+profilecurvature_crest = profilecurvature_crest.assign(metric = "profilecurvature", area = "crest")
+profilecurvature_crest.columns =  ['value' if x == 'MEAN' else x for x in profilecurvature_crest.columns]
+
 # 1. Merge tables
 list_dfs = [elevation_edge, elevation_crest, slope_edge, slope_crest,
-    slopeStd_edge, slopeStd_crest, rugosity_edge, rugosity_crest] # dfs
+    slopeStd_edge, slopeStd_crest, rugosity_edge, rugosity_crest,
+    profilecurvature_edge, profilecurvature_crest, planformcurvature_edge,
+    planformcurvature_crest] # dfs
+
 list_dfnames =  ["elevation_edge", "elevation_crest", "slope_edge", "slope_crest",
-    "slopeStd_edge", "slopeStd_crest", "rugosity_edge", "rugosity_crest"] # df names
+    "slopeStd_edge", "slopeStd_crest", "rugosity_edge", "rugosity_crest",
+    "planformcurvature_edge", "planformcurvature_crest", "profilecurvature_crest",
+    "profilecurvature_edge"] # df names
+
 df = pd.concat(list_dfs, keys = list_dfnames) # merge
 
-outputDf = tables + "\\metrics_joined.csv"
+resultFolder = scratchWS + "\\results" # make a folder for the result dataframe
+if not os.path.exists(resultFolder):
+    os.makedirs(resultFolder)
+
+outputDf = resultFolder + "\\metrics_joined.csv" # write out the result dataframe
 df.to_csv(outputDf)
 arcpy.AddMessage("data joined and exported to: " + outputDf)
